91cfabd8091bc78bc8f94c3b1fae88e0afacf05c
Rojer148 <ahmedsameh148889@gmail.com>
2019-09-28 04:56:33 +0200

    Task 1 Finished

---
+++ test.js
@@ -0,0 +1,63 @@
+var fs = require('fs');
+var rowData = fs.readFileSync('db.json');
+var members = JSON.parse(rowData);
+var Member = /** @class */ (function () {
+    function Member() {
+        this.firstName = "";
+        this.middleName = "";
+        this.lastName = "";
+        this.fullName = "";
+        this.committee = "";
+        this.joinDate = new Date();
+    }
+    return Member;
+}());
+var grid = [];
+var committees = [];
+var check = function (name) {
+    var i;
+    for (i = 0; i < committees.length; ++i) {
+        if (committees[i] === name)
+            return i;
+    }
+    return i;
+};
+for (var i = 0; i < 100; i++) {
+    grid[i] = [];
+}
+console.log(members.length);
+for (var i = 0; i < members.length; i++) {
+    var tmp = new Member();
+    tmp.committee = members[i].committee;
+    tmp.firstName = members[i].firstName;
+    tmp.joinDate = members[i].joinDate;
+    tmp.lastName = members[i].lastName;
+    tmp.middleName = members[i].middleName;
+    tmp.fullName = members[i].firstName + " " + members[i].middleName + " " + members[i].lastName;
+    var committe = tmp.committee;
+    var idx = check(committe);
+    if (grid[idx].length > 0) {
+        grid[idx].push(tmp);
+    }
+    else {
+        committees.push(committe);
+        grid[idx].push(tmp);
+    }
+}
+;
+for (var i = 0; i < grid.length; i++) {
+    if (grid[i].length <= 0)
+        continue;
+    console.log("\n======================== " + committees[i] + " ====================");
+    grid[i].sort(function (a, b) {
+        if (a.fullName < b.fullName)
+            return -1;
+        else if (a.fullName > b.fullName)
+            return 1;
+        else
+            return 0;
+    });
+    for (var y = 0; y < grid[i].length; y++) {
+        console.log((y + 1) + "- Name : " + grid[i][y].fullName + " , Joined In : " + grid[i][y].joinDate);
+    }
+}
---
+++ test.ts
@@ -0,0 +1,62 @@
+var fs = require('fs');
+
+var rowData = fs.readFileSync('db.json');
+
+var members = JSON.parse(rowData);
+
+class Member{
+    public firstName:string = "";
+    public middleName:string = "";
+    public lastName:string = "";
+    public fullName:string = "";
+    public committee:string = "";
+    public joinDate:Date = new Date();
+}
+
+let grid: Member[][] = [];
+let committees: string[] = [];
+
+var check = function (name:string){
+    let i;
+    for(i=0;  i<committees.length; ++i){
+        if(committees[i] === name)return i;
+    }
+    return i;
+}
+
+for(let i=0; i<100; i++){
+    grid[i]=[];
+}
+console.log(members.length);
+for(let i=0; i<members.length; i++) {
+    var tmp: Member = new Member();
+    tmp.committee = members[i].committee;
+    tmp.firstName = members[i].firstName;
+    tmp.joinDate = members[i].joinDate;
+    tmp.lastName = members[i].lastName;
+    tmp.middleName = members[i].middleName;
+    tmp.fullName = members[i].firstName + " " + members[i].middleName + " " + members[i].lastName;
+    var committe = tmp.committee;
+    
+    let idx: number = check(committe);
+    if(grid[idx].length > 0){
+        grid[idx].push(tmp);
+    }
+    else {
+        committees.push(committe);
+        grid[idx].push(tmp);
+    }
+};
+
+for(let i=0; i<grid.length; i++){
+    if(grid[i].length <= 0)continue;
+    console.log("\n======================== "+committees[i] + " ====================");
+    grid[i].sort((a,b)=>{
+        if(a.fullName < b.fullName)return -1;
+        else if(a.fullName > b.fullName)return 1;
+        else return 0;
+    });
+    for(let y=0; y<grid[i].length; y++){
+        console.log((y+1)+"- Name : "+grid[i][y].fullName+" , Joined In : "+grid[i][y].joinDate);
+    }
+}
